544e600300a989d4c791a7efd2e167cf55f423ec
petermusembi69 <nyongesamusembi@gmail.com>
2020-03-09 12:10:01 +0300

    first commit

---
+++ script.ts
@@ -0,0 +1,22 @@
+//Strings
+let myName = '10';
+//error
+// myName = 20 
+
+//number
+let num = 10;
+let hobby = false;
+// hobby = 1;
+
+let myRealAge;
+myRealAge = 25;
+myRealAge = 'oeoe';
+
+enum Color {
+    Blue,
+    Green,
+    Yellow
+}
+
+let myColor: Color = Color.Yellow;
+console.log(myColor);
